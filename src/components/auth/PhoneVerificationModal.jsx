c049ae0c7682b14ba74a08730f244508cc9061af
Kamal <cherkamal4@gmail.com>
2025-10-22 15:57:44 +0100

    Add figlogo.png

--- src/components/auth/PhoneVerificationModal.jsx
+++ src/components/auth/PhoneVerificationModal.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { QRCodeCanvas as QRCode } from "qrcode.react";
 import { Label } from "@/components/ui/label";
-import BrandLogo from "@/assets/figlogoai.png";
+import BrandLogo from "@/assets/figlogo.png";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Shield,
